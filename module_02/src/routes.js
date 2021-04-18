ecf42ee665eb1f6ef112fbdd0df8b40489ae4f62
wafx\erick.bezerra <freitas_erick@hotmail.com>
2021-04-18 15:41:28 -0300

    criando middleare de autenticação

--- module_02/src/routes.js
+++ module_02/src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMidleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.use(authMidleware);
+
+routes.put('/users/:id', UserController.update);
 
 export default routes;
