d42d29980bd9f2906dcdaafb8e5eefaccba5a8c1
erickfreitas <erickfreitasbezerra@gmail.com>
2021-04-25 12:29:39 -0300

    configurando sucrase e nodemon

---
+++ desafio_02/src/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+// import UserController from './app/controllers/UserController';
+// import SessionController from './app/controllers/SessionController';
+// import authMidleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+// routes.post('/sessions', SessionController.store);
+// routes.post('/users', UserController.store);
+
+// routes.use(authMidleware);
+
+// routes.put('/users', UserController.update);
+
+export default routes;
